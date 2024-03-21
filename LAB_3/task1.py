f54920bb452b987fc038f29283c0397d2d4fb978
exx0dusss <82311111+exx0dusss@users.noreply.github.com>
2024-03-21 15:58:33 +0100

    main branch modified

--- LAB_3/task1.py
+++
@@ -1,3 +0,0 @@
-# List Comprehensions
-listOfSquares = [x**2 for x in range(1, 11)]
-print(listOfSquares)
--- LAB_3/task2.py
+++
@@ -1,9 +0,0 @@
-# Functions
-def generate_squares(begin, end):
-    return [x ** 2 for x in range(begin, end + 1)]
-
-
-firstNum = 1
-secondNum = 10
-listOfSquares = generate_squares(firstNum, secondNum)
-print(listOfSquares)
