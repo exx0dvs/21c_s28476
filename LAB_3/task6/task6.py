33c11f4e45e48c83a6c135b8023dbad5ea7c6fd0
exx0dusss <82311111+exx0dusss@users.noreply.github.com>
2024-03-21 15:23:31 +0100

    task 6 finished

---
+++ LAB_3/task6/task6.py
@@ -0,0 +1,11 @@
+# Modules
+
+from square_generator import SquareGenerator
+
+firstNum = 1
+secondNum = 10
+generator = SquareGenerator()
+listOfSquares = generator.generate_squares(firstNum, secondNum)
+listOfRoots = generator.calculate_square_roots(listOfSquares)
+print(listOfSquares)
+print(listOfRoots)
