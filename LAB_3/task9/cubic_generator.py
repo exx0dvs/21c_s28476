5abde30cc6469b4a5471bd7477d0bbeac41c017c
exx0dusss <82311111+exx0dusss@users.noreply.github.com>
2024-03-21 15:36:00 +0100

    task 9 finished

--- LAB_3/task9/cubic_generator.py
+++ LAB_3/task9/cubic_generator.py
@@ -2,6 +2,11 @@ from task8.square_generator import SquareGenerator
 
 
 class CubicGenerator(SquareGenerator):
+    def generate_squares(self, begin, end):
+        if end < begin:
+            return super().generate_squares(begin, end)
+        return [x ** 2 for x in range(begin, end + 1)]
+
     def generate_cubes(self, begin, end):
         if end < begin:
             raise ValueError("End of range must be greater than or equal to the start.")
--- LAB_3/task9/square_generator.py
+++ LAB_3/task9/square_generator.py
@@ -3,8 +3,6 @@ import math
 
 class SquareGenerator:
     def generate_squares(self, begin, end):
-        if begin > end:
-            raise ValueError('end of the range is less than the start!')
         return [x ** 2 for x in range(begin, end + 1)]
 
     def calculate_square_roots(self, squares_list):
